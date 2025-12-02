c364e6ccb391861d68f7f8cc4bd4963d9d58197f
sklarmann <simon.klarmann@gmail.com>
2025-12-02 18:05:01 +0100

    Fix journal display conditional in author detail template

--- SimpleLiteratureManager/library/urls.py
+++ SimpleLiteratureManager/library/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("authors/", views.author_list, name="author_list"),
+    path("authors/<int:pk>/", views.author_detail, name="author_detail"),
     path("authors/add/", views.author_create, name="author_create"),
     path("authors/<int:pk>/edit/", views.author_update, name="author_update"),
     path("authors/<int:pk>/delete/", views.author_delete, name="author_delete"),
